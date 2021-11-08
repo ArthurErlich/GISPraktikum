b1f075dfe498bb0a5f3fe098f8d4274d8ffaacb1
ArthurErHFU <92059317+ArthurErHFU@users.noreply.github.com>
2021-11-08 13:53:29 +0100

    blub

---
+++ MainFolder/3_Aufgabe/JS/script_vorlage1.ts
@@ -0,0 +1,68 @@
+// -- [Aufgabe 1]
+
+/**
+ * @var {number} age: Bitte anstatt der 24 dein Alter eintragen
+ */
+let age: number = 24;
+
+/**
+ * @var {string} firstName: Bitte anstatt 'Max' deinen Vornamen eintragen
+ */
+let firstName: string = `Max`;
+
+function func1(age: number): number {
+  return 2021 - age;
+}
+
+let output: string = func2(firstName);
+
+function func3(meal?: string): string {
+  console.log(`Ich esse gerne ${meal || "Pizza"}.`);
+  return func1(age) > 1995
+    ? `Ich gehöre zur Generation Z`
+    : `Ich gehöre zur Generation Y`;
+}
+
+console.log(output);
+
+function func2(name: string): string {
+  console.log(`Ich heiße ${name}.`);
+  return func3();
+}
+
+/* -- HIER BITTE IHRE LÖSUNG ZUR AUFGABE 1 EINTRAGEN
+ * ...
+ */
+
+// -- [Aufgabe 2]
+
+let events: any[][] = [
+  ["Mark Knopfler", 10.1],
+  ["Pink Floyd", 15.9],
+  ["Metallica", 20.1],
+  ["Michael Bublé", 11.1],
+  ["Dire Straits", 12.2],
+  ["Mariah Carey", 1.1],
+  ["Cat Stevens", 12.99],
+  ["Mark Forster", 2.1],
+  ["Helene Fischer", 3.1],
+  ["Bee Gees", 25.2],
+];
+
+// -- HIER BITTE IHRE LÖSUNG ZUR AUFGABE 2 EINTRAGEN
+
+// Lösung a) ...
+
+// Lösung b) ...
+
+// Lösung c) ...
+
+// Lösung d) ...
+
+// Lösung e) ...
+
+// Lösung f) ...
+
+// Lösung g) ...
+
+// Lösung h) ...
