70c5a8b0ccfd84ec8b47a667c1b87ba03d95ea12
ArthurErHFU <92059317+ArthurErHFU@users.noreply.github.com>
2021-11-15 14:51:50 +0100

    Update Script.ts

--- MainFolder/4_Aufgabe/JS/Script.ts
+++ MainFolder/4_Aufgabe/JS/Script.ts
@@ -1,6 +1,6 @@
 namespace Aufgabe4{
 
-     //create html element --> creat a buldung stone (gif it an id // or save it on a array/list?)
+     //create html element --> creat a buldung stone (gif it an id // or save it on a array/list?) --Using timeAndDate for data-ToDoID
      //then apand it 
      //by clicking a button removing the appendet element
      //dont user innerhtml!!!
