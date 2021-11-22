8d81a18259979ec9fdbecb71188a840644312ef8
ArthurErHFU <92059317+ArthurErHFU@users.noreply.github.com>
2021-11-22 20:58:18 +0100

    Finished Lesson1

--- MainFolder/4_Aufgabe/JS/Script.js
+++ MainFolder/4_Aufgabe/JS/Script.js
@@ -1,56 +1,130 @@
 "use strict";
 var Aufgabe4;
 (function (Aufgabe4) {
+    class ToDoElement {
+        interpret;
+        price;
+        date;
+        constructor(interpret, price, date) {
+            this.interpret = interpret;
+            this.price = price;
+            this.date = date;
+        }
+    }
+    class ToDoElements {
+        elements = [];
+        addElement(element, index) {
+            console.log(this.elements);
+            this.elements[index] = element;
+        }
+        readElement(index) {
+            return this.elements[index];
+        }
+    }
     //create html element --> creat a buldung stone (gif it an id // or save it on a array/list?) --Using timeAndDate for data-ToDoID
     //then apand it 
     //by clicking a button removing the appendet element --> by getting the : article.dataset.NAME --> data-NAME addEventListener use bubling
     //dont user innerhtml!!!
     let addButton = document.getElementById("Button-Add");
-    let interpret;
-    let price;
-    let date;
+    let elementID = 0;
     addButton.addEventListener("click", addElement);
+    let toDoElements = new ToDoElements();
     function removeElement(event) {
-        let target = event.currentTarget;
-        target.parentElement.parentElement.parentElement.parentElement.remove();
+        let eventID = event.target.dataset.elementid;
+        let dataEvent = '[data-todu-elementid]';
+        let toDoElement = document.querySelectorAll(dataEvent);
+        //iterates thorug all elements end check to what needs to be removed
+        toDoElement.forEach(element => {
+            if (element.getAttribute("data-todu-elementid") == eventID) {
+                console.log(element + " with Tag: data-todu-elementid= " + eventID + " will be removed");
+                element.remove();
+                toDoElements.readElement(parseInt(eventID)) == null;
+            }
+        });
     }
     function addElement() {
+        elementID++;
         try {
-            interpret = document.getElementById("interpret_input").value;
-            price = parseInt(document.getElementById("price_input").value);
-            date = new Date(document.getElementById("datetime-local_input").value);
-            let table = document.createElement("table");
-            let tbody = document.createElement("tbody");
-            let toDoElment = document.createElement("tr");
-            let interpret_out = document.createElement("td");
-            let price_out = document.createElement("td");
-            let datetime_out = document.createElement("td");
-            let delet = document.createElement("td");
-            let deletButton = document.createElement("button");
-            toDoElment.classList.add("toDoElement");
-            interpret_out.classList.add("tnterpret_out");
-            interpret_out.innerText = interpret;
-            price_out.classList.add("price_out");
-            price_out.innerText = String(price);
-            datetime_out.classList.add("datetime_out");
-            datetime_out.innerText = date.getDay() + "." + date.getMonth() + "." + date.getFullYear() + "." + date.getHours() + date.getSeconds();
-            delet.classList.add("delet");
-            deletButton.classList.add("deletButton");
-            deletButton.setAttribute("type", "button");
-            deletButton.innerText = "X";
-            deletButton.addEventListener("click", removeElement, false);
-            toDoElment.appendChild(interpret_out);
-            toDoElment.appendChild(price_out);
-            toDoElment.appendChild(datetime_out);
-            delet.appendChild(deletButton);
-            toDoElment.appendChild(delet);
-            tbody.appendChild(toDoElment);
-            table.appendChild(tbody);
-            document.getElementById("toDoOUT").appendChild(table);
+            readForm();
         }
         catch (error) {
-            alert("Fehler bei der eingabe");
+            // alert(error);
+            return;
         }
+        createElement();
+        fillFrom();
+    }
+    function fillFrom() {
+        //get elemnt byID in this TAG
+        let toDoElement = document.getElementById(elementID.toString());
+        let interpret_out = toDoElement.getElementsByClassName("interpret_out");
+        let price_out = toDoElement.getElementsByClassName("price_out");
+        let datetime_out = toDoElement.getElementsByClassName("datetime_out");
+        console.log(interpret_out.item(0));
+        //Not a god way -> is ther a nother way?
+        interpret_out.item(0).textContent = toDoElements.readElement(elementID).interpret;
+        price_out.item(0).textContent = (toDoElements.readElement(elementID).price).toString();
+        datetime_out.item(0).textContent = (toDoElements.readElement(elementID).date).toTimeString();
+    }
+    function readForm() {
+        let interpret;
+        let price;
+        let date;
+        try {
+            interpret = document.getElementById("interpret_input").value;
+        }
+        catch (error) {
+            throw new Error("Interpret is empty!");
+        }
+        try {
+            price = parseInt(document.getElementById("price_input").value);
+        }
+        catch (error) {
+            throw new Error("Price is empty!");
+        }
+        try {
+            date = new Date(document.getElementById("datetime_local_input").value);
+        }
+        catch (error) {
+            throw new Error("Date is empty!");
+        }
+        console.log(interpret);
+        console.log(price);
+        console.log(date);
+        let toDoElement = new ToDoElement(interpret, price, date);
+        toDoElements.addElement(toDoElement, elementID);
+        console.log("Add to list " + toDoElements.readElement(elementID));
+    }
+    function createElement() {
+        let table = document.createElement("table");
+        let tbody = document.createElement("tbody");
+        let toDoElement = document.createElement("tr");
+        let interpret_out = document.createElement("td");
+        let price_out = document.createElement("td");
+        let datetime_out = document.createElement("td");
+        let delet = document.createElement("td");
+        let deletButton = document.createElement("button");
+        toDoElement.classList.add("toDoElement");
+        interpret_out.classList.add("interpret_out");
+        price_out.classList.add("price_out");
+        datetime_out.classList.add("datetime_out");
+        delet.classList.add("delet");
+        deletButton.classList.add("deletButton");
+        deletButton.innerText = "X" + elementID;
+        deletButton.setAttribute("type", "button");
+        deletButton.addEventListener("click", removeElement, false);
+        toDoElement.appendChild(interpret_out);
+        toDoElement.appendChild(price_out);
+        toDoElement.appendChild(datetime_out);
+        delet.appendChild(deletButton);
+        toDoElement.appendChild(delet);
+        tbody.appendChild(toDoElement);
+        table.appendChild(tbody);
+        let toDoOUT = document.getElementById("toDoOUT").appendChild(table);
+        console.log("createt ToDo elemtn wit the ID: " + elementID);
+        toDoOUT.setAttribute("data-todu-elementid", elementID + "");
+        deletButton.setAttribute("data-elementid", elementID + "");
+        toDoOUT.id = elementID.toString();
     }
 })(Aufgabe4 || (Aufgabe4 = {}));
 //# sourceMappingURL=Script.js.map
