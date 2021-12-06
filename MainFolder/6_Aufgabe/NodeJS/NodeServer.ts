3308f8dc45930bd7e6c315e9a18ccf167782ead1
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-07 00:51:19 +0100

    Update NodeServer.ts

--- MainFolder/6_Aufgabe/NodeJS/NodeServer.ts
+++ MainFolder/6_Aufgabe/NodeJS/NodeServer.ts
@@ -4,6 +4,23 @@ const hostname: string = 'localhost';
 const port: number = 3000;
 
 const server: http.Server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
+        response.statusCode = 200;
 
+        let url: URL = new URL(request.url);
+        switch (url.pathname) {
+            case "/":
+                response.write("HelloWorld");
+                break;
+            case "/convertDate":
+                response.write("ConvertDate");
+            default:
+                response.statusCode = 404;
+        }
+        response.end();
+    }
+);
 
-)
+server.listen(port, hostname); () => {
+    console.log('Server running at http://${hostnem}:${port}/');
+};
