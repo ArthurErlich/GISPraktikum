b32b37e118f36834978e60aa8ef344d960395c94
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-07 00:51:17 +0100

    Update Script.ts

--- MainFolder/6_Aufgabe/JS/Script.ts
+++ MainFolder/6_Aufgabe/JS/Script.ts
@@ -1,3 +1,8 @@
 namespace Aufgabe6 {
-
+    async function request(url: RequestInfo): Promise<string> {
+        let response: Response = await fetch(url);
+        let text: string = await response.text();
+        return text;
+    }
 }
+
