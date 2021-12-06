9c11f560497db3203b184481cd2613074cc1d863
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-07 00:10:46 +0100

    Added NodeJS Modules

---
+++ MainFolder/6_Aufgabe/NodeJS/NodeServer.ts
@@ -0,0 +1,10 @@
+import * as http from "http";
+
+const hostname1: string = 'localhost';
+const hostname2: string = '192.0.0.1';
+const port: number = 3000;
+
+const server: http.Server = http.createServer(
+
+
+)
