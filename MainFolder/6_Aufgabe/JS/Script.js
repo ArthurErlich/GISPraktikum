d86df2e977d5d7777d0e66676a1cb7eb7f619a57
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-08 00:45:35 +0100

    Update Script.js

--- MainFolder/6_Aufgabe/JS/Script.js
+++ MainFolder/6_Aufgabe/JS/Script.js
@@ -1,6 +1,12 @@
 "use strict";
 var Aufgabe6;
 (function (Aufgabe6) {
+    let answer = document.getElementById("answer");
+    let sendRequest = document.getElementById("sendRequest");
+    let sendDate = document.getElementById("sendDate");
+    let dateElment = document.getElementById("date_local_input");
+    sendRequest.addEventListener("click", getStatus);
+    sendDate.addEventListener("click", getDate);
     async function request(url) {
         let response = await fetch(url);
         let text = await response.text();
@@ -14,21 +20,13 @@ var Aufgabe6;
         let text = await response.text();
         return text;
     }
-    let answer = document.getElementById("answer");
-    let sendRequest = document.getElementById("sendRequest");
-    let sendDate = document.getElementById("sendDate");
-    sendRequest.addEventListener("click", getText);
-    sendDate.addEventListener("click", getDate);
-    async function getText() {
-        console.log(await request("http://localhost:3000/"));
+    async function getStatus() {
+        answer.textContent = await request("http://localhost:3000/");
     }
-    async function getDate(event) {
-        console.log(event + " Request sent!");
-        let text = document.createElement("p");
-        text.id = "fDate";
-        text.textContent = await requestPost("http://localhost:3000/convertDate", new Date());
-        console.log("request received");
-        answer.appendChild(text);
+    async function getDate() {
+        let text = await requestPost("http://localhost:3000/convertDate", new Date(dateElment.value));
+        answer.textContent = text;
+        console.log(text);
     }
 })(Aufgabe6 || (Aufgabe6 = {}));
 //# sourceMappingURL=Script.js.map
