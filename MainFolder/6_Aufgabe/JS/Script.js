a3c5203db141bee673ab113c0d000504528f0896
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-08 15:49:15 +0100

    Update Script.js

--- MainFolder/6_Aufgabe/JS/Script.js
+++ MainFolder/6_Aufgabe/JS/Script.js
@@ -1,17 +1,23 @@
 "use strict";
 var Aufgabe6;
 (function (Aufgabe6) {
-    let answer = document.getElementById("answer");
-    let sendRequest = document.getElementById("sendRequest");
-    let sendDate = document.getElementById("sendDate");
-    let dateElment = document.getElementById("date_local_input");
+    const answer = document.getElementById("answer");
+    const sendRequest = document.getElementById("sendRequest");
+    const postDateElement = document.getElementById("postDate");
+    const getDateElemnt = document.getElementById("getDate");
+    const dateElment = document.getElementById("date_local_input");
+    const url = "http://localhost:3050/";
+    const urlPost = "convertDate/post";
+    const urlGet = "convertDate/get";
     sendRequest.addEventListener("click", getStatus);
-    sendDate.addEventListener("click", getDate);
+    getDateElemnt.addEventListener("click", getDate);
+    postDateElement.addEventListener("click", postDate);
     async function request(url) {
         let response = await fetch(url);
         let text = await response.text();
         return text;
     }
+    //GET METHODE
     async function requestPost(url, data) {
         let response = await fetch(url, {
             method: "post",
@@ -20,11 +26,25 @@ var Aufgabe6;
         let text = await response.text();
         return text;
     }
+    //POST METHODE
+    async function requestGet(url, data) {
+        let response = await fetch(url + "?a=" + JSON.stringify(data), {
+            method: "get",
+        });
+        let text = await response.text();
+        return text;
+    }
     async function getStatus() {
-        answer.textContent = await request("http://localhost:3000/");
+        answer.textContent = await request(url);
+    }
+    //Post
+    async function postDate() {
+        let text = await requestPost(url + urlPost, new Date(dateElment.value));
+        answer.textContent = text;
+        console.log(text);
     }
     async function getDate() {
-        let text = await requestPost("http://localhost:3000/convertDate", new Date(dateElment.value));
+        let text = await requestGet(url + urlGet, new Date(dateElment.value));
         answer.textContent = text;
         console.log(text);
     }
