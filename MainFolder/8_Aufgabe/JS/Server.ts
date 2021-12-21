d45b56e68170d0fc671423ece53b8bd76bc6ae94
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-21 10:44:03 +0100

    Added MongDB node dependeies

--- MainFolder/8_Aufgabe/JS/Server.ts
+++ MainFolder/8_Aufgabe/JS/Server.ts
@@ -2,4 +2,14 @@ namespace Aufgabe8 {
     import *as mongo from "mongodb";
 
     let url: string = "mongodb://localhost:27017" // localer MongoDB Server!
+    let options: string;
+
+    let mongoClient: mongo.MongoClient = new mongo.MongoClient(url, options);
+
+    async function connectDB() {
+        await mongoClient.connect();
+    }
+    async function disconnecttDB() {
+        await mongoClient.close();
+    }
 }
