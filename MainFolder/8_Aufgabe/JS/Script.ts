68bef15437e0386ebfcb433612eb3a8ea25d9ddc
ArthurErHFU <arthur.erlich@hs-furtwangen.de>
2021-12-25 13:18:39 +0100

    Client TS edit

--- MainFolder/8_Aufgabe/JS/Script.ts
+++ MainFolder/8_Aufgabe/JS/Script.ts
@@ -1,4 +1,37 @@
 namespace Aufgabe8 {
 
-    const
+    interface TodoElement{
+        id:number;
+        interpret:string;
+        price:number;
+        date:Date;
+    }
+
+    //load end check if something is in the database
+
+    let todoFrom: HTMLFormElement = <HTMLFormElement>(document.getElementById("toDoForm"));
+
+    todoFrom.addEventListener("submit", onSubmint);
+    
+
+    async function onSubmint(event:Event){
+        event.preventDefault();
+        let formData: FormData = new FormData(<HTMLFormElement>event.currentTarget);
+
+        let id : number  =parseFloat((<HTMLElement>event.currentTarget).dataset.id);
+
+        let interpret : string = formData.get("");
+        let price : number = formData.get("");
+        let date :Date = formData.get("");
+        
+
+        let todoElement :TodoElement = {
+            id,
+            interpret,
+            price,
+            date
+        };
+    }
+
+    //fetsh post and get -> create new if id is empty if not edit current
 }
